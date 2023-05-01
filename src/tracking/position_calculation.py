c84c2bf25eaed94b433c4b1efe7c3e1a05fdbaa6
dKenez <47757462+dKenez@users.noreply.github.com>
2023-05-01 18:09:48 +0200

    Merged position calc and stereo object matching

--- src/tracking/position_calculation.py
+++ src/tracking/position_calculation.py
@@ -1,35 +1,39 @@
 import numpy as np
 
-def position_calculation(ul, vl, ur, vr, c_mat=None, frame="camera"):
+
+def position_calculation(
+    ul, vl, ur, vr, c_mat: np.ndarray | None = None, frame="camera"
+):
     """
     Find object position (center of box) wrt Left camera frame (Cam2) or Car frame, returns a 3D position vector
     """
 
-
     # Camera intrinsic parameters
-    c_mat = c_mat or [[1228.8600498339667, 0.0, 687.0567266957923], [0.0, 1074.1759184503744, 254.13182034905003], [0.0, 0.0, 1.0]]
+    c_mat = c_mat or np.array(
+        [
+            [1228.8600498339667, 0.0, 687.0567266957923],
+            [0.0, 1074.1759184503744, 254.13182034905003],
+            [0.0, 0.0, 1.0],
+        ]
+    )
 
-    fx = c_mat[0,0]
-    fy = c_mat[1,1]
-    ox = c_mat[0,2]
-    oy = c_mat[1,2]
+    fx = c_mat[0, 0]
+    fy = c_mat[1, 1]
+    ox = c_mat[0, 2]
+    oy = c_mat[1, 2]
 
     b = 0.54
 
-    v_avg = (vl + vr)/2
+    v_avg = (vl + vr) / 2
 
     # Calculate position wrt Cam 2 (left) frame
-    z_cam = b*fx/(ur-ul)
-    x_cam = z_cam*(ul-ox)/fx
-    y_cam = z_cam*(v_avg-oy)/fy
+    z_cam = b * fx / (ur - ul)
+    x_cam = z_cam * (ul - ox) / fx
+    y_cam = z_cam * (v_avg - oy) / fy
     obj_cam = np.array([x_cam, y_cam, z_cam, 1])
 
-
     # Tranformation matrix between Cam 2 (left) and Car frames
-    Tcam_car = np.array([[1, 0, 0, 0.06],
-                    [0, 1, 0, 1.65],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1]])
+    Tcam_car = np.array([[1, 0, 0, 0.06], [0, 1, 0, 1.65], [0, 0, 1, 0], [0, 0, 0, 1]])
 
     # Position wrt Car frame
     obj_car = np.dot(Tcam_car, obj_cam.T)
--- src/tracking/run_tracking.py
+++ src/tracking/run_tracking.py
@@ -1,68 +1,144 @@
 import click
 import cv2
+import numpy as np
 from rich import print
 import torch
 from pathlib import Path
 from skimage import io
+import pandas as pd
+from src.tracking.position_calculation import position_calculation
 
-from src.tracking.stereo_object_matching import match_objects
+from src.tracking.stereo_object_matching import draw_matching_bbox, match_objects
 
 root_dir = Path(__file__).resolve().parents[2]
-data_dir = root_dir / 'data'
-model_dir = root_dir / 'models'
+data_dir = root_dir / "data"
+model_dir = root_dir / "models"
+
 
 @click.command()
 @click.option(
     "--seq",
-    default="raw/final_project_2023_rect/seq_03",
+    default=data_dir / "raw/final_project_2023_rect/seq_03",
     prompt="Choose the video sequence: ",
     help="Choose sequence.",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
 )
-@click.option(
-    "--calib_file",
-    default="processed/calib.json",
-    prompt="Choose calibration file for camera images: ",
-    help="The person to greet.",
-)
-def run_tracking(seq, calib_file):
+# @click.option(
+#     "--calib_file",
+#     default=data_dir / "processed/calib.json",
+#     prompt="Choose calibration file for camera images: ",
+#     help="The person to greet.",
+#     type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+# )
+def run_tracking(seq: Path):
     """Run tracking on video sequence from data folder."""
 
     print()
     print(f"Sequence path: {seq}")
-    print(f"Calibration file: {calib_file}")
+    print()
 
     print("[bold blue]Loading model...[/bold blue]")
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_dir / 'best.pt', force_reload=True)
+    model = torch.hub.load(
+        "ultralytics/yolov5", "custom", path=model_dir / "best.pt", force_reload=True
+    )
     model.eval()
-    print("[bold blue]Model loaded[/bold blue]")
+    print("[bold blue]Model loaded[/bold blue]\n")
 
-    img_left_path = data_dir / "raw/final_project_2023_rect/seq_03/image_02/data/0000000000.png"  # or file, Path, PIL, OpenCV, numpy, list
-    img_right_path = data_dir / "raw/final_project_2023_rect/seq_03/image_03/data/0000000000.png"  # or file, Path, PIL, OpenCV, numpy, list
+    print("[bold blue]Finding frames[/bold blue]")
 
-    # Inference
-    results = model([img_left_path, img_right_path])
+    dp_left = seq / "image_02/data"
+    dp_right = seq / "image_03/data"
 
-    # Results
-    r = results.pandas().xyxy
+    glob_left = sorted(dp_left.glob("*.png"))
+    glob_right = sorted(dp_right.glob("*.png"))
 
-    results_left_df = r[0]
-    results_right_df = r[1]
+    print(glob_left[0])
 
-    row_ind, col_ind = match_objects(results_left_df, results_right_df, img_left_path, img_right_path)
-    
+    print(f"Found {len(glob_left)} frames\n")
 
-    # frame = io.imread(img_right)
-    # for _, box in r[0].iterrows():
-    #     if box["class"]==0:
-    #         xB = int(box["xmin"])
-    #         yB = int(box["ymin"])
-    #         xA = int(box["xmax"])
-    #         yA = int(box["ymax"])
-    #         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    # empty dataframe
+    object_columns = [
+        "frame",
+        "object_id",
+        "object_type",
+        "position_x",
+        "position_y",
+        "position_z",
+        "last_seen",
+        "bbox_cx",
+        "bbox_cy",
+        "bbox_w",
+        "bbox_h",
+    ]
+    objects_df = pd.DataFrame(columns=object_columns)
+    for frame, (img_left_path, img_right_path) in enumerate(zip(glob_left, glob_right)):
+        print(f"[bold white]Processing frame [bold blue]{frame}[/bold blue]: [bold blue]{img_left_path.name}[/bold blue][/bold white]")
 
-    # io.imsave("test.png", frame)
+        # img_left_path = (
+        #     data_dir / "raw/final_project_2023_rect/seq_03/image_02/data/0000000000.png"
+        # )  # or file, Path, PIL, OpenCV, numpy, list
+        # img_right_path = (
+        #     data_dir / "raw/final_project_2023_rect/seq_03/image_03/data/0000000000.png"
+        # )  # or file, Path, PIL, OpenCV, numpy, list
+
+        img_left = io.imread(img_left_path)
+        img_right = io.imread(img_right_path)
 
 
+
+        # Inference
+        results = model([img_left, img_right])
+
+        # Results
+        r_xyxy = results.pandas().xyxy
+        r_xywh = results.pandas().xywh
+
+        results_left_df = r_xyxy[0]
+        results_right_df = r_xyxy[1]
+
+        row_ind, col_ind = match_objects(
+            results_left_df, results_right_df, img_left, img_right
+        )
+
+        draw_matching_bbox(
+            results_left_df, results_right_df, img_left, img_right, row_ind, col_ind
+        )
+
+        results_left_df = r_xywh[0]
+        results_right_df = r_xywh[1]
+
+        for idx_left, idx_right in zip(row_ind, col_ind):
+            box_left = results_left_df.iloc[idx_left]
+            box_right = results_right_df.iloc[idx_right]
+
+            x, y, z = position_calculation(
+                box_left["xcenter"],
+                box_left["ycenter"],
+                box_right["xcenter"],
+                box_right["ycenter"],
+            )
+            obj_id = np.random.randint(20)
+            new_row_df = pd.DataFrame(
+                [
+                    [
+                        frame,
+                        obj_id,
+                        box_left["name"],
+                        x,
+                        y,
+                        z,
+                        0,
+                        box_left["xcenter"],
+                        box_left["ycenter"],
+                        box_left["width"],
+                        box_right["height"],
+                    ]
+                ],
+                columns=object_columns,
+            )
+            objects_df = pd.concat([objects_df, new_row_df], ignore_index=True)
+            pass
+        objects_df.to_csv("test.csv")
 
 
 if __name__ == "__main__":
